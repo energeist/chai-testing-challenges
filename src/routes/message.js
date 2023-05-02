632fda4609a365ff18c0b1e4b2eb000d1c12e76e
Mark Rattle <mark.rattle@gmail.com>
2023-05-02 11:52:50 -0400

    I hate it

--- src/routes/message.js
+++ src/routes/message.js
@@ -7,7 +7,12 @@ const Message = require('../models/message')
 /** Route to get all messages. */
 router.get('/', (req, res) => {
     // TODO: Get all Message objects using `.find()`
-
+    Message.find().then((messages) => {
+        return res.json({messages})
+    })
+    .catch((err) => {
+        throw err.message
+    });
     // TODO: Return the Message objects as a JSON list
 })
 
@@ -15,7 +20,12 @@ router.get('/', (req, res) => {
 router.get('/:messageId', (req, res) => {
     // TODO: Get the Message object with id matching `req.params.id`
     // using `findOne`
-
+    Message.findOne({_id: req.params.messageId})
+    .then(result => {
+        res.json(result)
+    }).catch(err => {
+        throw err.message
+    })
     // TODO: Return the matching Message object as JSON
 })
 
@@ -32,7 +42,7 @@ router.post('/', (req, res) => {
         return user.save()
     })
     .then(() => {
-        return res.send(message)
+        return res.json({message})
     }).catch(err => {
         throw err.message
     })
@@ -41,16 +51,30 @@ router.post('/', (req, res) => {
 /** Route to update an existing message. */
 router.put('/:messageId', (req, res) => {
     // TODO: Update the matching message using `findByIdAndUpdate`
-
-    // TODO: Return the updated Message object as JSON
+    Message.findByIdAndUpdate(req.params.messageId, req.body).then(() => {
+        return Message.findOne({_id: req.params.messageId})
+    }).then((message) => {
+        // TODO: Return the updated Message object as JSON
+        return res.json({message})
+    }).catch((err) => {
+        throw err.message
+    })
 })
 
 /** Route to delete a message. */
-router.delete('/:messageId', (req, res) => {
-    // TODO: Delete the specified Message using `findByIdAndDelete`. Make sure
-    // to also delete the message from the User object's `messages` array
-
-    // TODO: Return a JSON object indicating that the Message has been deleted
+router.delete('/:messageId', async (req, res) => {
+    Message.findByIdAndDelete(req.params.messageId).then((messageResult) => {
+        if (messageResult === null) {
+            return res.json({message: 'Message does not exist.'})
+        }
+        return res.json({
+            'message': 'Successfully deleted.',
+            '_id': req.params.messageId
+        })
+    })
+    .catch((err) => {
+        throw err.message
+    })
 })
 
 module.exports = router
